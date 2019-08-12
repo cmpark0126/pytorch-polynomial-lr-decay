26a4838b56500cbfde808cb671478f8c855b2d94
Park Chun Myong <cmpark0126@gmail.com>
2019-08-12 12:34:58 +0900

    Update run.py

--- torch_poly_lr_decay/run.py
+++ torch_poly_lr_decay/run.py
@@ -6,7 +6,7 @@ from torch_poly_lr_decay import PolynomialLRDecay
 if __name__ == '__main__':
     v = torch.zeros(10)
     optim = torch.optim.SGD([v], lr=0.01)
-    scheduler = PolynomialLRDecay(optim, max_decay_steps=19, power=2.0)
+    scheduler = PolynomialLRDecay(optim, max_decay_steps=19, end_learning_rate=0.0001, power=2.0)
 
     for epoch in range(1, 20):
         scheduler.step(epoch)
